c12fa3122de9ed9c9e17c75684bd90d552881661
Kevin Bertiz <111998096+KevinBertiz7@users.noreply.github.com>
2024-06-12 09:40:54 -0500

    Add files via upload

---
+++ Datos/ConexionBD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class ConexionBD
+    {
+        protected SqlConnection conexion;
+        protected string cadenaConexion;
+
+        public ConexionBD()
+        {
+            this.cadenaConexion = "Data Source=LAPTOP-IB1AKTDE\\SQLEXPRESS;Initial Catalog=rentCar;Integrated Security=True";
+            this.conexion = new SqlConnection(this.cadenaConexion);
+        }
+
+        public string AbrirConnexion()
+        {
+            try
+            {
+                this.conexion.Open();
+                return "Conexión abierta";
+            }
+            catch (Exception ex)
+            {
+                return "Error al abrir la conexión: " + ex.Message;
+            }
+        }
+
+        public void CerrarConnexion()
+        {
+            if (this.conexion.State == System.Data.ConnectionState.Open)
+            {
+                this.conexion.Close();
+            }
+        }
+    }
+}
---
+++ Datos/ICRUD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entidades;
+namespace DatosDAO
+{
+    public interface ICRUD<T>
+    {
+        string Insertar(T obj);
+        string Eliminar(T obj);
+        string Actualizar(T obj);
+        T BuscarID(string id);
+        List<T> Todos(string obj);
+        List<T> TodosFiltro(string obj);
+    }
+}
