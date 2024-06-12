698685a45feaecf383790ccb4d0e6c5965d6631d
Kevin Bertiz <111998096+KevinBertiz7@users.noreply.github.com>
2024-06-12 09:42:28 -0500

    Add files via upload

---
+++ Logica/ICRUD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public interface ICRUD<T>
+    {
+        string Insertar(T obj);
+        string Eliminar(T obj);
+        string Actualizar(T obj);
+        T BuscarID(string id);
+        List<T> Todos(string obj);
+        List<T> TodosFiltro(string obj);
+    }
+}
---
+++ Logica/ServicioVehiculo.cs
@@ -0,0 +1,43 @@
+﻿using Datos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class ServicioVehiculo : ICRUD<Vehiculo>
+    {
+        public string Actualizar(Vehiculo obj)
+        {
+            return new RepositorioVehiculo().Actualizar(obj);
+        }
+
+        public Vehiculo BuscarID(string id)
+        {
+            return new RepositorioVehiculo().BuscarID(id);
+        }
+
+        public string Eliminar(Vehiculo obj)
+        {
+            return new RepositorioVehiculo().Eliminar(obj);
+        }
+
+        public string Insertar(Vehiculo obj)
+        {
+            return new RepositorioVehiculo().Insertar(obj);
+        }
+
+        public List<Vehiculo> Todos(string obj)
+        {
+            return new RepositorioVehiculo().Todos(obj);
+        }
+
+        public List<Vehiculo> TodosFiltro(string condicion)
+        {
+            return new RepositorioVehiculo().TodosFiltro(condicion);
+        }
+    }
+}
